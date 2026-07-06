9ba59b6ce57a826e38c875ba1c83644fa8546385
Sniffr <malingu.sidney@studnets.jkuat.ac.ke>
2026-07-06 19:02:59 +0300

    feat: add show_in_store flag + _store_services helper

--- addons/custom_appointments/tests/__init__.py
+++ addons/custom_appointments/tests/__init__.py
@@ -2,3 +2,4 @@ from . import test_feedback
 from . import test_appointment_source
 from . import test_requires_specific_staff
 from . import test_feedback_settings_fixes
+from . import test_service_store
